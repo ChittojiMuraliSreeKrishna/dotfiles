085982055926ccb6e6e425d9b60fc43c15acf3d3
ChittojiMuraliSreeKrishna <cmkrishna97@gmail.com>
2021-05-27 09:19:26 +0530

    New update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,6 @@ set -g -x fish_greeting 'Welcome wargun'
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /home/wargun/Documents/Codes/Anaconda/bin/conda "shell.fish" "hook" $argv | source
+# eval /home/wargun/Documents/Codes/Anaconda/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
